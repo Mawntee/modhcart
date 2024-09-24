b89e5e4af0d5fa8025298e42d4ddcb90acacf6b9
Swifter <swifter1243@gmail.com>
2024-09-23 22:17:04 -0400

    real rlelalr eal

---
+++ external.js
@@ -0,0 +1,16 @@
+function balls(x)
+{
+    return x * 2
+}
+
+function balls2(x)
+{
+    return x * 0.5
+}
+
+// module.exports.balls = balls
+
+module.exports = {
+    balls,
+    balls2
+}
---
+++ main.js
@@ -0,0 +1,3 @@
+const external = require('./external.js')
+
+console.log(external.balls2(2))
